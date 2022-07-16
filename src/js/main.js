fa59b9bf85facb9554cb7746a3d82bf29901fba0
MartaRodriguezOnis <marta.ronis@gmail.com>
2022-07-16 13:40:53 +0200

    Add fovourites, events and render in html

--- src/js/main.js
+++ src/js/main.js
@@ -8,6 +8,44 @@ const resultUl = document.querySelector('.js_resultUl');
 const favouritetUl = document.querySelector('.js_favouritetUl');
 
 let data = [];
+let favourites = [];
+
+//Traemos todos los li de resultados para hacer click en ellos y que ocurra algo (se añadan al array favoritos)
+
+const renderAnimeFav = () => {
+  let html = '';
+  for (const eachFav of favourites) {
+    html += `<li class="js_liAnime" id="${eachFav.mal_id}">`;
+    html += `<img src="${eachFav.images.jpg.image_url}" alt="Imagen del anime ${eachFav.title}">`;
+    html += `<h2>${eachFav.title}</h2>`;
+    html += `</li>`;
+  }
+  favouritetUl.innerHTML = html;
+  listenerAnime();
+};
+
+function handleClickFav(event) {
+  const idSelect = parseInt(event.currentTarget.id);
+  console.log(`idSElect current target id: ${event.currentTarget.id}`);
+  console.log(`data : ${data}`);
+  const animeClick = data.find((anime) => anime.mal_id === idSelect);
+
+  const favouriteClick = favourites.findIndex((fav) => fav.mal_id === idSelect);
+  if (favouriteClick === -1) {
+    favourites.push(animeClick);
+  } else {
+    favourites.splice(favouriteClick, 1);
+  }
+  console.log(`favoritos : ${favourites}`);
+  // Pintar
+  renderAnimeFav();
+}
+const listenerAnime = () => {
+  const liAnime = document.querySelectorAll('.js_liAnime');
+  for (const li of liAnime) {
+    li.addEventListener('click', handleClickFav);
+  }
+};
 const renderAnimeResult = () => {
   let html = '';
   for (const eachAnimeData of data) {
@@ -23,8 +61,10 @@ const renderAnimeResult = () => {
 
     html += `<h2>${eachAnimeData.title}</h2>`;
     html += `</li>`;
+    console.log(typeof eachAnimeData.mal_id);
   }
   resultUl.innerHTML = html;
+  listenerAnime();
 };
 
 function handleClick(event) {
@@ -35,6 +75,7 @@ function handleClick(event) {
     .then((response) => response.json())
     .then((json) => {
       data = json.data;
+      console.log(data);
       renderAnimeResult();
     });
 }
